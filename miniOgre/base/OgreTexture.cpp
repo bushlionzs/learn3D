59e8d6988f157ab617478a1288c532aaaf66efd0
shazhou <shazhou@gizaworks.com>
2024-01-15 09:57:33 +0800

    add file

---
+++ miniOgre/base/OgreTexture.cpp
@@ -0,0 +1,284 @@
+#include "OgreHeader.h"
+#include "OgreTexture.h"
+#include "myutils.h"
+#include "OgreResourceManager.h"
+#include "OgreBitwise.h"
+
+namespace Ogre {
+	ITexture::ITexture(const String& name, TextureProperty* texProperty)
+	{
+		mName = name;
+		if (texProperty)
+		{
+			mTextureProperty = *texProperty;
+		}
+
+		mUsage = mTextureProperty._tex_usage;
+		mFormat = mTextureProperty._tex_format;
+
+		mFace = 1;
+
+		mNumMipmaps = 100;
+	}
+
+	ITexture::~ITexture()
+	{
+
+	}
+
+	TextureType ITexture::getTextureType()
+	{
+		return mTextureProperty._texType;
+	}
+
+	const HardwarePixelBufferPtr& ITexture::getBuffer(size_t face, size_t mipmap)
+	{
+		size_t idx = face * (mTextureProperty._numMipmaps + 1) + mipmap;
+		assert(idx < mSurfaceList.size());
+		return mSurfaceList[idx];
+	}
+
+	void ITexture::createInternalResources()
+	{
+		if (mInternalResourcesCreated)
+			return;
+		createInternalResourcesImpl();
+		mInternalResourcesCreated = true;
+	}
+
+	void ITexture::freeInternalResources(void)
+	{
+		if (mInternalResourcesCreated)
+		{
+			mSurfaceList.clear();
+			freeInternalResourcesImpl();
+			mInternalResourcesCreated = false;
+		}
+	}
+
+	void ITexture::preLoad()
+	{
+		
+	}
+
+	void ITexture::postLoad()
+	{
+
+	}
+
+	void ITexture::loadImpl()
+	{
+		if ((mUsage & TU_RENDERTARGET) || mUsage == TU_DYNAMIC_WRITE_ONLY)
+		{
+			mFormat = mTextureProperty._tex_format;
+			createInternalResources();
+			return;
+		}
+
+		
+
+
+		bool cube = isCubeTexture();
+		if (cube)
+		{
+			mFace = 6;
+		}
+
+
+		std::vector<std::string> names;
+		std::vector<CImage> images;
+		names.reserve(mFace);
+
+		if (cube)
+		{
+			std::string suffix = getSuffix(mName);
+			if (ResourceManager::getSingletonPtr()->hasResource(mName, BLANKSTRING))
+			{
+				names.push_back(mName);
+				images.emplace_back();
+			}
+			else
+			{
+				std::string basename = removeSuffix(mName);
+				std::string currname;
+				const char* CUBEMAP_SUFFIXES[] = { "_rt", "_lf", "_up", "_dn", "_fr", "_bk" };
+				const char* CUBEMAP_SUFFIXES_ALT[] = { "_px", "_nx", "_py", "_ny", "_pz", "_nz" };
+				for (int32_t i = 0; i < 6; i++)
+				{
+					currname = basename + CUBEMAP_SUFFIXES[i] + suffix;
+					if (ResourceManager::getSingletonPtr()->hasResource(currname, BLANKSTRING))
+					{
+						names.push_back(currname);
+					}
+					else
+					{
+						currname = basename + CUBEMAP_SUFFIXES_ALT[i] + suffix;
+						names.push_back(currname);
+					}
+				}
+			}
+
+		}
+		else
+		{
+			names.push_back(mName);
+		}
+
+		images.resize(names.size());
+
+		bool b = true;
+		for (int32_t i = 0; i < names.size(); i++)
+		{
+			if (!images[i].loadImage(names[i]))
+			{
+				b = false;
+			}
+		}
+
+		if (!b)
+		{
+			return;
+		}
+
+		std::vector<const CImage*> imagePtrs;
+		imagePtrs.resize(images.size());
+
+		for (int32_t i = 0; i < images.size(); i++)
+		{
+			imagePtrs[i] = &images[i];
+		}
+		_loadImages(imagePtrs);
+	}
+
+	bool ITexture::load()
+	{
+		if (mLoad)
+			return true;
+		preLoad();
+		loadImpl();
+		postLoad();
+		mLoad = true;
+		return true;
+	}
+
+	uint32 ITexture::getMaxMipmaps() const
+	{
+		// see ARB_texture_non_power_of_two
+		return Bitwise::mostSignificantBitSet(
+			std::max(mTextureProperty._width, 
+				std::max(mTextureProperty._height, mTextureProperty._depth)));
+	}
+
+	void ITexture::loadImage(const CImage& img)
+	{
+		_loadImages({ &img });
+	}
+
+	void ITexture::_loadImages(const std::vector<const CImage*>& images)
+	{
+		mSrcWidth = images[0]->getWidth();
+		mSrcHeight = images[0]->getHeight();
+		mSrcDepth = images[0]->getDepth();
+		mSrcFormat = images[0]->getFormat();
+
+		mTextureProperty._width = mSrcWidth;
+		mTextureProperty._height = mSrcHeight;
+		mTextureProperty._depth = mSrcDepth;
+		mTextureProperty._tex_format = mSrcFormat;
+		mTextureProperty._numMipmaps = images[0]->getNumMipmaps();
+		mNumMipmaps = mTextureProperty._numMipmaps;
+
+		if (PixelUtil::isCompressed(mSrcFormat) && mNumMipmaps > 0)
+		{
+			mNumMipmaps = std::max((uint32_t)1, mNumMipmaps);
+			mTextureProperty._numMipmaps = mNumMipmaps;
+		}
+
+		mFormat = PixelUtil::getFormatForBitDepths(mSrcFormat, 0, 0);
+		mFace = 1;
+		if (isCubeTexture())
+		{
+			mFace = 6;
+		}
+		createInternalResources();
+
+
+		uint32 faces;
+		bool multiImage; // Load from multiple images?
+		if (images.size() > 1)
+		{
+			faces = uint32(images.size());
+			multiImage = true;
+		}
+		else
+		{
+			faces = images[0]->getNumFaces();
+			multiImage = false;
+		}
+
+		
+		int32_t mip = 0;
+		int32_t depth = 1;
+		for (uint32 mip = 0; mip <= std::min(mNumMipmaps, mTextureProperty._numMipmaps); ++mip)
+		{
+			for (uint32 i = 0; i < mFace; ++i)
+			{
+				PixelBox src;
+				size_t face = (depth == 1) ? i : 0; // depth = 1, then cubemap face else 3d/ array layer
+
+				auto buffer = getBuffer(face, mip);
+				Box dst(0, 0, 0, buffer->getWidth(), buffer->getHeight(), buffer->getDepth());
+
+				if (multiImage)
+				{
+					// Load from multiple images
+					src = images[i]->getPixelBox(0, mip);
+					// set dst layer
+					if (depth > 1)
+					{
+						dst.front = i;
+						dst.back = i + 1;
+					}
+				}
+				else
+				{
+					// Load from faces of images[0]
+					src = images[0]->getPixelBox(i, mip);
+				}
+
+				if (mTextureProperty._gamma != 1.0f) {
+					assert(false);
+					//// Apply gamma correction
+					//// Do not overwrite original image but do gamma correction in temporary buffer
+					//Image tmp(src.format, src.getWidth(), getHeight(), src.getDepth());
+					//PixelBox corrected = tmp.getPixelBox();
+					//PixelUtil::bulkPixelConversion(src, corrected);
+
+					//Image::applyGamma(corrected.data, mGamma, tmp.getSize(), tmp.getBPP());
+
+					//// Destination: entire texture. blitFromMemory does the scaling to
+					//// a power of two for us when needed
+					//buffer->blitFromMemory(corrected, dst);
+				}
+				else
+				{
+					// Destination: entire texture. blitFromMemory does the scaling to
+					// a power of two for us when needed
+					buffer->blitFromMemory(src, dst);
+				}
+			}
+		}
+	}
+
+	void ITexture::unload()
+	{
+		assert(false);
+	}
+
+	void ITexture::loadRawData(DataStreamPtr& stream, ushort uWidth, ushort uHeight, PixelFormat format)
+	{
+		CImage img;
+		img.loadRawData(stream, uWidth, uHeight, format);
+		loadImage(img);
+	}
+}
---
+++ miniOgre/base/OgreTexture.h
@@ -0,0 +1,200 @@
+#pragma once
+#include "OgreHardwareBuffer.h"
+#include "OgrePixelFormat.h"
+#include "OgreHardwarePixelBuffer.h"
+#include "OgreImage.h"
+namespace Ogre {
+    enum TextureTypePbr
+    {
+        TextureTypePbr_general = 0,
+        TextureTypePbr_Albedo = 1,
+        TextureTypePbr_AmbientOcclusion = 2,
+        TextureTypePbr_NormalMap = 3,
+        TextureTypePbr_Emissive = 4,
+        TextureTypePbr_MetalRoughness = 5,
+        TextureTypePbr_BRDF_LUT = 6,
+        TextureTypePbr_IBL_Diffuse = 7,
+        TextureTypePbr_IBL_Specular = 8
+    };
+    
+
+    enum TextureType : uint8_t
+    {
+        /// 1D texture, used in combination with 1D texture coordinates
+        TEX_TYPE_1D = 1,
+        /// 2D texture, used in combination with 2D texture coordinates (default)
+        TEX_TYPE_2D = 2,
+        /// 3D volume texture, used in combination with 3D texture coordinates
+        TEX_TYPE_3D = 3,
+        /// cube map (six two dimensional textures, one for each cube face), used in combination with 3D
+        /// texture coordinates
+        TEX_TYPE_CUBE_MAP = 4,
+        /// 2D texture array
+        TEX_TYPE_2D_ARRAY = 5,
+        /// GLES2 only OES texture type
+        TEX_TYPE_EXTERNAL_OES = 6
+    };
+
+    enum TextureUsage
+    {
+        /// same as #HBU_GPU_TO_CPU
+        TU_STATIC = HBU_GPU_TO_CPU,
+        /// same as #HBU_CPU_ONLY
+        TU_DYNAMIC = HBU_CPU_ONLY,
+        /// same as #HBU_DETAIL_WRITE_ONLY
+        TU_WRITE_ONLY = HBU_DETAIL_WRITE_ONLY,
+        /// same as #HBU_GPU_ONLY
+        TU_STATIC_WRITE_ONLY = HBU_GPU_ONLY,
+        /// same as #HBU_CPU_TO_GPU
+        TU_DYNAMIC_WRITE_ONLY = HBU_CPU_TO_GPU,
+        /// Mipmaps will be automatically generated for this texture
+        TU_AUTOMIPMAP = 0x10,
+        /** This texture will be a render target, i.e. used as a target for render to texture
+            setting this flag will ignore all other texture usages except TU_AUTOMIPMAP, TU_UAV, TU_NOT_SRV */
+        TU_RENDERTARGET = 0x20,
+        /// Texture would not be used as Shader Resource View, i.e. as regular texture.
+        /// That flag could be combined with TU_RENDERTARGET or TU_UAV to remove possible limitations on some hardware
+        TU_NOT_SRV = 0x40,
+        /// Texture can be bound as an Unordered Access View
+        /// (imageStore/imageRead/glBindImageTexture in GL jargon)
+        TU_UAV = 0x80,
+        /// Texture can be used as an UAV, but not as a regular texture.
+        TU_UAV_NOT_SRV = TU_UAV | TU_NOT_SRV,
+        /// Default to automatic mipmap generation static textures
+        TU_DEFAULT = TU_AUTOMIPMAP | HBU_GPU_ONLY,
+    };
+
+    class TextureProperty
+    {
+    public:
+        TextureType _texType = TEX_TYPE_2D;
+        TextureTypePbr _pbrType = TextureTypePbr_general;
+        uint32_t _width = 0;
+        uint32_t _height = 0;
+        uint32_t _depth = 1;
+        uint32_t _numMipmaps = 0;
+        Ogre::PixelFormat _tex_format = Ogre::PF_R8G8B8A8;
+        float _gamma = 1.0f;
+        uint32_t _fsaa = 0;
+        TextureUsage _tex_usage = TU_DEFAULT;
+        ColourValue _backgroudColor = ColourValue::Black;
+        TextureAddressingMode  _tex_addr_mod = Ogre::TAM_WRAP;
+    };
+
+    class ITexture
+    {
+    public:
+        ITexture(const String& name, TextureProperty* texProperty);
+        ~ITexture();
+        const String& getName()
+        {
+            return mName;
+        }
+
+        void setWidth(int32_t width)
+        {
+            mTextureProperty._width = width;
+        }
+
+        int32_t getWidth()
+        {
+            return mTextureProperty._width;
+        }
+
+        void setHeight(int32_t height)
+        {
+            mTextureProperty._height = height;
+        }
+
+        void setNumMipmaps(uint32 num)
+        {
+            mNumMipmaps = num;
+        }
+
+        int32_t getHeight()
+        {
+            return mTextureProperty._height;
+        }
+
+        int32_t getDepth()
+        {
+            return mTextureProperty._depth;
+        }
+
+        uint32_t getSize()
+        {
+            return PixelUtil::getMemorySize(mTextureProperty._width, mTextureProperty._height,
+                mTextureProperty._depth, mTextureProperty._tex_format);
+        }
+
+        Ogre::PixelFormat getFormat()
+        {
+            return mTextureProperty._tex_format;
+        }
+
+        uint32_t getSourceMipmaps()
+        {
+            return mTextureProperty._numMipmaps;
+        }
+        uint32_t getNumMipmaps()
+        {
+            return mNumMipmaps;
+        }
+
+        uint32_t getFace()
+        {
+            return mFace;
+        }
+
+        bool isCubeTexture()
+        {
+            return mTextureProperty._texType == TEX_TYPE_CUBE_MAP;
+        }
+        bool isLoaded()
+        {
+            return mLoad;
+        }
+
+        TextureProperty* getTextureProperty()
+        {
+            return &mTextureProperty;
+        }
+
+        TextureType getTextureType();
+
+        virtual const HardwarePixelBufferPtr& getBuffer(size_t face = 0, size_t mipmap = 0);
+        void createInternalResources();
+        void freeInternalResources(void);
+        virtual void createInternalResourcesImpl(void) = 0;
+        virtual void freeInternalResourcesImpl(void) = 0;
+        virtual void preLoad();
+        bool load();
+        void loadImpl();
+        virtual void postLoad();
+        uint32 getMaxMipmaps() const;
+        void loadImage(const CImage& img);
+        void _loadImages(const std::vector<const CImage*>& images);
+        virtual void unload();
+
+        void loadRawData(DataStreamPtr& stream, ushort uWidth, ushort uHeight, PixelFormat format);
+    protected:
+        String mName;
+        TextureProperty mTextureProperty;
+        uint32_t mFace = 1;
+        uint32_t mNumMipmaps = 0;
+        PixelFormat mFormat;
+        int mUsage;
+
+        PixelFormat mSrcFormat;
+        uint32 mSrcWidth, mSrcHeight, mSrcDepth;
+
+        std::vector<String> mLayerNames;
+
+        typedef std::vector<HardwarePixelBufferPtr> SurfaceList;
+        SurfaceList mSurfaceList;
+
+        bool mLoad = false;
+
+        bool mInternalResourcesCreated = false;
+    };
+}
